6e7d19dd3cdb59ef8f34995f887067e6e0c8748e
Hennoshin <m.irfan.hilmi@gmail.com>
2023-05-18 14:30:39 +0800

    Change memory view

--- lib/flowchart_editor/views/memory_view.dart
+++ lib/flowchart_editor/views/memory_view.dart
@@ -1,6 +1,7 @@
 import 'package:code_chart/flowchart_editor/execution_environment/data_types.dart';
 import 'package:code_chart/flowchart_editor/execution_environment/memory.dart';
 import 'package:code_chart/flowchart_editor/view_models/execution_viewmodel.dart';
+import 'package:code_chart/flowchart_editor/view_models/memory_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,29 +10,59 @@ class MemoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ExecutionViewModel? vm = context.watch<ExecutionViewModel?>();
-    List<Widget> widgets = [const SizedBox(
-      height: 50,
-      child: Center(child: Text("Empty"))
-    )];
-    if (vm != null) {
-      widgets.clear();
-
-      Memory memory = vm.environment!.topStack;
-      for (MapEntry<String, Wrapper> item in memory.variables.entries) {
-        String varName = item.key;
-        dynamic value = item.value.value ?? "Uninitialized";
-
-        widgets.add(SizedBox(
-            height: 50,
-            child: Center(child: Text("$varName = $value"))
-        ));
-      }
-    }
-
-    return ListView(
-      children: widgets,
+    return Selector<MemoryViewModel, List<Memory>>(
+      selector: (_, vm) => vm.memory,
+      builder: (_, list, __) => list.isEmpty ?
+      const Text("Memory empty") :
+      ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) => ExpansionTile(
+          title: Text(list[index].stackName),
+          children: <Widget>[
+            ListView.builder(
+              itemCount: list[index].variables.length,
+              itemBuilder: (_, index) => _VariableEntry(
+                  varName: list[index].variables.keys.elementAt(index),
+                  stackIndex: index
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
+}
 
+class _VariableEntry extends StatelessWidget {
+  final String varName;
+  final int stackIndex;
+
+  const _VariableEntry({super.key, required this.varName, required this.stackIndex});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: 200,
+      decoration: BoxDecoration(
+          border: Border.all()
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+                border: Border(
+                    bottom: BorderSide()
+                )
+            ),
+            child: Text(varName),
+          ),
+          Selector<MemoryViewModel, dynamic>(
+            selector: (_, vm) => vm.memory[stackIndex].getData(varName).value,
+            builder: (_, value, __) => Text(value.toString()),
+          )
+        ],
+      ),
+    );
+  }
 }
