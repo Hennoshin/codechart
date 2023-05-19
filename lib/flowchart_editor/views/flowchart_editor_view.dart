d33b5a0229650103afdc146f115a730d3bebcd00
Hennoshin <m.irfan.hilmi@gmail.com>
2023-05-19 18:42:50 +0800

    Add execution control row

--- lib/flowchart_editor/views/flowchart_editor_view.dart
+++ lib/flowchart_editor/views/flowchart_editor_view.dart
@@ -1,11 +1,8 @@
-import 'package:code_chart/flowchart_editor/models/base_element.dart';
-import 'package:code_chart/flowchart_editor/models/branching_element.dart';
-import 'package:code_chart/flowchart_editor/models/flowchart.dart';
-import 'package:code_chart/flowchart_editor/models/merging_element.dart';
-import 'package:code_chart/flowchart_editor/view_models/element_viewmodel.dart';
 import 'package:code_chart/flowchart_editor/view_models/flowchart_editor_viewmodel.dart';
+import 'package:code_chart/flowchart_editor/view_models/flowchart_viewmodel.dart';
+import 'package:code_chart/flowchart_editor/view_models/memory_viewmodel.dart';
+import 'package:code_chart/flowchart_editor/views/flowchart_view.dart';
 import 'package:code_chart/flowchart_editor/views/memory_view.dart';
-import 'package:code_chart/flowchart_editor/widgets/element_widget.dart';
 import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 
@@ -31,7 +28,21 @@ class _FlowchartEditorViewState extends State<FlowchartEditorView> {
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(10.0),
-          child: _FlowchartModelView()
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ChangeNotifierProxyProvider<FlowchartEditorViewModel, FlowchartViewModel>(
+                create: (_) => FlowchartViewModel(viewModel.currentFlowchart),
+                update: (_, flowchartEditorViewModel, fvm) => fvm!..update(flowchartEditorViewModel),
+                child: const FlowchartView(),
+              ),
+              ChangeNotifierProxyProvider<FlowchartEditorViewModel, MemoryViewModel>(
+                create: (_) => MemoryViewModel(),
+                update: (_, flowchartEditorViewModel, memoryVm) => memoryVm!..update(flowchartEditorViewModel),
+                child: const MemoryView(),
+              )
+            ],
+          )
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -64,6 +75,68 @@ class _FlowchartEditorViewState extends State<FlowchartEditorView> {
   }
 
 }
+
+class _ToolsRow extends StatelessWidget {
+  const _ToolsRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<FlowchartEditorViewModel, Set<int>>(
+      selector: (_, vm) => { vm.selectedToolsIndex },
+      builder: (context, selectedIndex, _) {
+        return Row(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.delete),
+              color: selectedIndex.single == 0 ? Colors.red : Colors.grey,
+              onPressed: () {
+                context.read<FlowchartEditorViewModel>().selectedToolsIndex = 0;
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.edit),
+              color: selectedIndex.single == 1 ? Colors.blue : Colors.grey,
+              onPressed: () {
+                context.read<FlowchartEditorViewModel>().selectedToolsIndex = 1;
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+}
+
+class _FlowchartExecutionControl extends StatelessWidget {
+  const _FlowchartExecutionControl({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<FlowchartEditorViewModel, bool>(
+      selector: (_, vm) => vm.isFlowchartRunning,
+      builder: (_, isRunning, __) => Row(
+        children: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.play_arrow),
+            color: Colors.green,
+            onPressed: () {
+              context.read<FlowchartEditorViewModel>().stepRunFlowchart();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.stop),
+            color: isRunning ? Colors.red : null,
+            onPressed: isRunning ? () {
+              context.read<FlowchartEditorViewModel>().stopFlowchart();
+            } : null,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/*
 
 class _FlowchartModelView extends StatelessWidget {
   _FlowchartModelView({Key? key}) : super(key: key);
@@ -138,62 +211,4 @@ class _AddButton extends StatelessWidget {
 
 }
 
-class _ToolsRow extends StatelessWidget {
-  const _ToolsRow({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Selector<FlowchartEditorViewModel, Set<int>>(
-      selector: (_, vm) => { vm.selectedToolsIndex },
-      builder: (context, selectedIndex, _) {
-        return Row(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.delete),
-              color: selectedIndex.single == 0 ? Colors.red : Colors.grey,
-              onPressed: () {
-                context.read<FlowchartEditorViewModel>().selectedToolsIndex = 0;
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.edit),
-              color: selectedIndex.single == 1 ? Colors.blue : Colors.grey,
-              onPressed: () {
-                context.read<FlowchartEditorViewModel>().selectedToolsIndex = 1;
-              },
-            )
-          ],
-        );
-      }
-    );
-  }
-}
-
-class _FlowchartExecutionControl extends StatelessWidget {
-  const _FlowchartExecutionControl({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Selector<FlowchartEditorViewModel, bool>(
-      selector: (_, vm) => vm.isFlowchartRunning,
-      builder: (_, isRunning, __) => Row(
-        children: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.play_arrow),
-            color: Colors.green,
-            onPressed: () {
-              context.read<FlowchartEditorViewModel>().stepRunFlowchart();
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.stop),
-            color: isRunning ? Colors.red : null,
-            onPressed: isRunning ? () {
-              context.read<FlowchartEditorViewModel>().stopFlowchart();
-            } : null,
-          )
-        ],
-      ),
-    );
-  }
-}
+ */
