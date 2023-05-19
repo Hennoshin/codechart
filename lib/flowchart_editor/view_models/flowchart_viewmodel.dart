043e123338ca14fe599b520a545b6114218e0af0
Hennoshin <m.irfan.hilmi@gmail.com>
2023-05-19 18:45:15 +0800

    Update Flowchart View Model to sync on the Editor View Model

--- lib/flowchart_editor/view_models/flowchart_viewmodel.dart
+++ lib/flowchart_editor/view_models/flowchart_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:code_chart/flowchart_editor/models/base_element.dart';
+import 'package:code_chart/flowchart_editor/view_models/flowchart_editor_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 import '../models/flowchart.dart';
@@ -7,6 +8,12 @@ class FlowchartViewModel extends ChangeNotifier {
   Flowchart _flowchart;
 
   FlowchartViewModel(this._flowchart);
+
+  void update(FlowchartEditorViewModel flowchartEditorViewModel) {
+    _flowchart = flowchartEditorViewModel.currentFlowchart;
+
+    notifyListeners();
+  }
 
   void addNewElement(BaseElement newElement, String location) {
     _flowchart.addElement2(newElement, location);
@@ -18,6 +25,14 @@ class FlowchartViewModel extends ChangeNotifier {
     _flowchart.removeElement2(location);
 
     notifyListeners();
+  }
+
+  BaseElement? elementAt(String pos) {
+    return _flowchart.elements2[pos];
+  }
+
+  int flowchartElementCount() {
+    return _flowchart.elements2.length;
   }
 
   Flowchart get flowchart => _flowchart;
