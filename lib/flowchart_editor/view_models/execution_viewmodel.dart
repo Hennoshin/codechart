5971cd18071e206b5ffb05cf16b43ce6ff795a73
Hennoshin <m.irfan.hilmi@gmail.com>
2023-05-19 18:36:36 +0800

    Update Execution View Model

--- lib/flowchart_editor/view_models/execution_viewmodel.dart
+++ lib/flowchart_editor/view_models/execution_viewmodel.dart
@@ -1,9 +1,27 @@
 import 'package:code_chart/flowchart_editor/execution_environment/execution_environment.dart';
+import 'package:code_chart/flowchart_editor/models/base_element.dart';
 import 'package:flutter/material.dart';
 
+import 'flowchart_editor_viewmodel.dart';
+
 class ExecutionViewModel extends ChangeNotifier {
-  ExecutionEnvironment? environment;
+  ExecutionEnvironment? _environment;
+  BaseElement? currentElement;
 
-  ExecutionViewModel(this.environment);
+  ExecutionViewModel(this._environment);
 
+  void update(FlowchartEditorViewModel vm) {
+    var env = vm.mainProgram.runEnv;
+    _environment = env;
+    currentElement = _environment?.currentElement;
+
+    notifyListeners();
+  }
+
+  ExecutionEnvironment? get environment => _environment;
+  set environment(ExecutionEnvironment? env) {
+    _environment = env;
+
+    notifyListeners();
+  }
 }
