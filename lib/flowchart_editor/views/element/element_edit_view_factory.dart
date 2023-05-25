b7305681ca98d8f5bef71ebac1d5f80f116da9ce
Hennoshin <m.irfan.hilmi@gmail.com>
2023-05-25 22:55:11 +0800

    Add factory for the element edit view

---
+++ lib/flowchart_editor/views/element/element_edit_view_factory.dart
@@ -0,0 +1,28 @@
+import 'package:code_chart/flowchart_editor/models/assignment_element.dart';
+import 'package:code_chart/flowchart_editor/models/declaration_element.dart';
+import 'package:code_chart/flowchart_editor/models/output_element.dart';
+import 'package:code_chart/flowchart_editor/views/element/declaration_element_edit_view.dart';
+import 'package:code_chart/flowchart_editor/views/element/output_element_edit_view.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/base_element.dart';
+import 'assignment_element_edit_view.dart';
+
+class ElementEditViewFactory {
+  const ElementEditViewFactory();
+
+  @factory
+  Widget? createElementEditView(BaseElement element) {
+    if (element is DeclarationElement) {
+      return DeclarationElementEditView(element: element);
+    }
+    if (element is AssignmentElement) {
+      return AssignmentElementEditView(element: element);
+    }
+    if (element is OutputElement) {
+      return OutputElementEditView(element: element);
+    }
+
+    return null;
+  }
+}
