5332ffa9cd5bab2077a27a3da1ac30a22ec533e5
Hennoshin <m.irfan.hilmi@gmail.com>
2023-06-01 19:48:21 +0800

    Add input text box

--- lib/flowchart_editor/views/console_view.dart
+++ lib/flowchart_editor/views/console_view.dart
@@ -1,10 +1,17 @@
+import 'package:code_chart/flowchart_editor/view_models/flowchart_editor_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../view_models/console_viewmodel.dart';
 
 class ConsoleView extends StatelessWidget {
-  const ConsoleView({super.key});
+  final TextEditingController _inputController = TextEditingController();
+
+  ConsoleView({super.key});
+
+  void _inputToConsole(BuildContext context) {
+    context.read<FlowchartEditorViewModel>().setInputBuffer(_inputController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +20,34 @@ class ConsoleView extends StatelessWidget {
       width: 500,
       child: Consumer<ConsoleViewModel>(
         builder: (_, consoleViewModel, __) =>
-            ListView.builder(
-                itemCount: consoleViewModel.outputBuffer.length,
-                itemBuilder: (context, index) => Container(
-                  height: 750,
-                  width: 450,
-                  child: Text(consoleViewModel.outputBuffer[index]),
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: consoleViewModel.outputBuffer.length,
+                      itemBuilder: (context, index) => Container(
+                        height: 500,
+                        width: 450,
+                        child: Text(consoleViewModel.outputBuffer[index]),
+                      )
+                  )
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 450,
+                      child: TextField(controller: _inputController)
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.send),
+                      color: Colors.blue,
+                      onPressed: consoleViewModel.expectingInput ? () {
+                        _inputToConsole(context);
+                      } : null,
+                    )
+                  ],
                 )
+              ],
             )
       ),
     );
