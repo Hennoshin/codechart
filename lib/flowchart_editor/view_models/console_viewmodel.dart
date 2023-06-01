ab71a5c06cdf2d0abbedde0a3a0cbeeb117cacaa
Hennoshin <m.irfan.hilmi@gmail.com>
2023-06-01 19:48:45 +0800

    Add properties to check expecting input

--- lib/flowchart_editor/view_models/console_viewmodel.dart
+++ lib/flowchart_editor/view_models/console_viewmodel.dart
@@ -4,21 +4,25 @@ import 'package:flutter/material.dart';
 class ConsoleViewModel extends ChangeNotifier {
   List<String>? _outputBuffer;
   int _bufferLength = 0;
+  bool _expectingInput = false;
 
   ConsoleViewModel();
 
   void update(FlowchartEditorViewModel flowchartEditorViewModel) {
     List<String>? buffer = flowchartEditorViewModel.mainProgram.runEnv?.outputBuffer;
+    bool expectInput = flowchartEditorViewModel.mainProgram.runEnv?.isExpectingInput ?? false;
     int length = buffer?.length ?? 0;
 
-    if (length == _bufferLength) {
+    if (length == _bufferLength && _expectingInput == expectInput) {
       return;
     }
 
     _outputBuffer = buffer;
     _bufferLength = length;
+    _expectingInput = expectInput;
     notifyListeners();
   }
 
   List<String> get outputBuffer => _outputBuffer ?? [];
+  bool get expectingInput => _expectingInput;
 }
