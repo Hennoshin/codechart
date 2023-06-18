99c63f5334920007f24359a35d3e21c112ec49a8
Hennoshin <m.irfan.hilmi@gmail.com>
2023-06-18 12:32:46 +0800

    Update factory

--- lib/flowchart_editor/views/element/element_edit_view_factory.dart
+++ lib/flowchart_editor/views/element/element_edit_view_factory.dart
@@ -1,10 +1,14 @@
 import 'package:code_chart/flowchart_editor/models/assignment_element.dart';
+import 'package:code_chart/flowchart_editor/models/branching_element.dart';
 import 'package:code_chart/flowchart_editor/models/declaration_element.dart';
 import 'package:code_chart/flowchart_editor/models/input_element.dart';
 import 'package:code_chart/flowchart_editor/models/output_element.dart';
+import 'package:code_chart/flowchart_editor/models/while_loop_element.dart';
+import 'package:code_chart/flowchart_editor/views/element/branching_element_edit_view.dart';
 import 'package:code_chart/flowchart_editor/views/element/declaration_element_edit_view.dart';
 import 'package:code_chart/flowchart_editor/views/element/input_element_edit_view.dart';
 import 'package:code_chart/flowchart_editor/views/element/output_element_edit_view.dart';
+import 'package:code_chart/flowchart_editor/views/element/while_loop_element_edit_view.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/base_element.dart';
@@ -26,6 +30,12 @@ class ElementEditViewFactory {
     }
     if (element is InputElement) {
       return InputElementEditView(element: element);
+    }
+    if (element is WhileLoopElement) {
+      return WhileLoopElementEditView(element: element);
+    }
+    if (element is BranchingElement) {
+      return BranchingElementEditView(element: element);
     }
 
     return null;
