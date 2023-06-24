e88e2bddba7197b8bdcaaa99d3dbac4e8642a4a1
Hennoshin <m.irfan.hilmi@gmail.com>
2023-06-24 22:17:08 +0800

    Change logic of setters and getters for FunctionFlowchart

--- lib/flowchart_editor/models/function_flowchart.dart
+++ lib/flowchart_editor/models/function_flowchart.dart
@@ -1,5 +1,6 @@
 import 'package:code_chart/flowchart_editor/execution_environment/data_types.dart';
 import 'package:code_chart/flowchart_editor/models/flowchart.dart';
+import 'package:code_chart/flowchart_editor/models/terminal_element.dart';
 
 class FunctionArg {
   String name;
@@ -10,13 +11,69 @@ class FunctionArg {
 }
 
 class FunctionFlowchart extends Flowchart {
-  List<FunctionArg> argList = [];
-  DataType? returnType;
-  String name;
+  final List<FunctionArg> _argList = [];
+  DataType? _returnType;
+  String _name;
 
-  FunctionFlowchart(this.name) : super(name);
+  FunctionFlowchart(this._name) : super(_name);
 
-  void setReturn(String expr) {
-    elements[1].baseExpr = expr;
+  void addFunctionParameter(String name, DataType type, [bool isArray = false]) {
+    if (argList.indexWhere((element) => element.name == name) != -1) {
+      throw Exception("Failed to add new parameter, the name already existed");
+    }
+    argList.add(FunctionArg(name, type, isArray));
+  }
+
+  void changeFunctionParameter(int index, String newName, DataType newType, bool newIsArray) {
+    FunctionArg parameter = argList[index];
+
+    int existingIndex = argList.indexWhere((element) => element.name == newName);
+    if (existingIndex != -1 && existingIndex != index) {
+      throw Exception("Failed to change parameter, the name already existed");
+    }
+
+    parameter.name = newName;
+    parameter.type = newType;
+    parameter.isArray = newIsArray;
+  }
+
+  String get name => _name;
+  set name(String n) {
+    _name = n;
+
+    String index = (elements2.keys.toList()..sort((str1, str2) => str1.compareTo(str2))).first;
+    (elements2[index]! as TerminalElement).placeholder = n;
+  }
+
+  DataType? get returnType => _returnType;
+  set returnType(type) {
+    if (type == null) {
+      returnExpression = null;
+    }
+
+    _returnType = type;
+  }
+
+  String get returnExpression {
+    String index = (elements2.keys.toList()..sort((str1, str2) => str1.compareTo(str2))).last;
+
+    return elements2[index]!.baseExpr ?? "";
+  }
+  set returnExpression(String? expr) {
+    if (_returnType == null) {
+      return;
+    }
+
+    String index = (elements2.keys.toList()..sort((str1, str2) => str1.compareTo(str2))).last;
+
+    elements2[index]!.baseExpr = expr;
+  }
+
+  List<FunctionArg> get argList => _argList;
+  set argList(List<FunctionArg> lst) {
+    argList.clear();
+    for (FunctionArg arg in lst) {
+      addFunctionParameter(arg.name, arg.type, arg.isArray);
+    }
   }
 }
