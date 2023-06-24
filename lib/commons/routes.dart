9318671ebd07815d805a9b23dabdb7c338287868
Hennoshin <m.irfan.hilmi@gmail.com>
2023-06-24 22:14:53 +0800

    Change route names to literal constant

--- lib/commons/routes.dart
+++ lib/commons/routes.dart
@@ -1,11 +1,26 @@
 import 'package:code_chart/flowchart_editor/screens/flowchart_editor.dart';
 import 'package:flutter/material.dart';
 
+import '../flowchart_editor/screens/function_edit_screen.dart';
+import '../flowchart_editor/screens/function_manager.dart';
+
 Route? pageRouting(RouteSettings settings) {
   switch (settings.name) {
-    case "/home":
-      return MaterialPageRoute(builder: (context) => FlowchartEditorScreen());
+    case RouteNames.home:
+      return MaterialPageRoute(builder: (context) => FlowchartEditorScreen(settings: settings));
+
+    case RouteNames.functionManager:
+      return MaterialPageRoute(builder: (_) => FunctionManagerScreen(settings: settings));
+      
+    case RouteNames.functionEdit:
+      return MaterialPageRoute(builder: (_) => FunctionEditScreen(settings: settings));
   }
   
   return null;
+}
+
+class RouteNames {
+  static const home = "/home";
+  static const functionManager = "/function-manager";
+  static const functionEdit = "$functionManager/edit";
 }
