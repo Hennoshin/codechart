9ff237bfd6464a19f124da57380cece1a4943f7f
Hennoshin <m.irfan.hilmi@gmail.com>
2023-07-25 00:42:19 +0800

    Add command to save the code to file

--- lib/code_converter/view_models/code_conversion_viewmodel.dart
+++ lib/code_converter/view_models/code_conversion_viewmodel.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:code_chart/code_converter/model/converted_codes.dart';
-import 'package:code_chart/flowchart_editor/models/flowchart.dart';
 import 'package:code_chart/flowchart_editor/models/flowchart_program.dart';
+import 'package:code_chart/utility/file_io_service.dart';
 import 'package:flutter/foundation.dart';
 
 class CodeConversionViewModel extends ChangeNotifier {
@@ -8,5 +10,11 @@ class CodeConversionViewModel extends ChangeNotifier {
 
   CodeConversionViewModel(FlowchartProgram program) : codes = ConvertedCodes(program);
 
+  Future<void> saveCode() async {
+    FileIOService service = FileIOService.instance;
 
+    await service.saveToFile(fileName: "${codes.program.programName}.cpp", bytes: Uint8List.fromList(utf8.encode(codes.toString())), mime: "text/plain");
+  }
+
+  String get rawCodes => codes.sourceCode.join("\n");
 }
